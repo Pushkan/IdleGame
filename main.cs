077c9b50786540136637703efe10d7740a75d6c4
Paul <pushkan13@gmail.com>
2020-03-04 15:05:02 +0000

    Исправлена ошибка, когда расход объекта начинал превышать его доход. Добавлен вывод ошибок.

--- main.cs
+++ main.cs
@@ -3,6 +3,8 @@ using System.Threading;
 
 class MainClass {
   
+  static string textError;
+
   //Текущие координаты курсора
   static int origCol;
   static int origRow;
@@ -10,27 +12,25 @@ class MainClass {
   static int enterCol = 1;
   static int enterRow = 6;
 
-  static double cash = 50;
-  //static bool working = false;
+  static double cash = 10050;
 
   static double salary = 0;
   static double taxes = 0;
-  static double generalTaxe = 1;
   
   //Obligation
   static int obligations = 0;
   static double plusObligation = 1.2;
   static double minusObligation = 0.95;
-  static double kObligation = 0.995;
+  static double kObligation = 0.986;
   static double currentKObligation = 1;
   static double costObligation = 20;
   //Papers
   static int papers = 0;
   static double plusPapers = 5.6;
   static double minusPapers = 4.4;
-  static double kPapers = 0.98;
-  static double currentKPaper = 1;
-  static double costPaper = 100;
+  static double kPapers = 0.991;
+  static double currentKPapers = 1;
+  static double costPapers = 100;
 
   static void Main(string[] args)
   {
@@ -45,7 +45,7 @@ class MainClass {
       // устанавливаем метод обратного вызова
       TimerCallback tm = new TimerCallback(Count);
       // создаем таймер
-      Timer timer = new Timer(tm, num, 0, 400);      
+      Timer timer = new Timer(tm, num, 0, 40);      
 
       while(cash > 0)
       {
@@ -61,14 +61,13 @@ class MainClass {
       
       if(cash > 0)
       {
-        CheckSalary();
-        CheckTaxes();
+        CheckSalaryAndTaxes();
         cash += salary - taxes;
 
         origRow = Console.CursorTop;
         origCol = Console.CursorLeft;
-        WriteAt($"$ {cash:f2}  + {salary:f2}  - {taxes:f2}", 1, 1);
-        WriteAt($"1 - Купить облигацию ${costObligation:f2} (+{currentKObligation * plusObligation :f2}, -{(2 - currentKObligation) * minusObligation :f2}); 2 - Купить ценные бумаги ${costPaper:f2} (+{currentKPaper * plusPapers :f2}, -{(2 - currentKPaper) * minusPapers :f2})", 1, 3);
+        WriteAt($"$ {cash:f2}  + {salary:f2}  - {taxes:f2}  {textError}", 1, 1);
+        WriteAt($"1 - Купить облигацию ${costObligation:f2} (+{currentKObligation * plusObligation :f2}, -{(2 - currentKObligation) * minusObligation :f2}); 2 - Купить ценные бумаги ${costPapers:f2} (+{currentKPapers * plusPapers :f2}, -{(2 - currentKPapers) * minusPapers :f2})", 1, 3);
         WriteAt("Введите команду", 1, 4);
       }
       else
@@ -103,63 +102,75 @@ class MainClass {
 
   static void Game()
   {
-    // if(!working)
-    // {
       
-      string asd = Console.ReadLine();
-      
-      if(asd == "1")
+    string command = Console.ReadLine();
+    
+    if(command == "1")
+    {
+      cash -= costObligation;        
+      obligations++;
+      double tempCurrentKObligation = Math.Pow(kObligation, obligations);
+      if(plusObligation * tempCurrentKObligation > minusObligation * (2 - tempCurrentKObligation))
       {
-        cash -= costObligation;        
-        obligations++;
-        currentKObligation = Math.Pow(kObligation, obligations);
-        costObligation *= (2 - currentKObligation);
-        ClearConsoleLine(enterRow);
-        Console.SetCursorPosition(enterCol, enterRow);
+        currentKObligation = tempCurrentKObligation;
       }
-      else if(asd == "2")
+      costObligation *= (2 - currentKObligation);
+      ClearConsoleLine(enterRow);
+      Console.SetCursorPosition(enterCol, enterRow);
+    }
+    else if(command == "2")
+    {
+      cash -= costPapers;        
+      papers++;
+      double tempCurrentKPapers = Math.Pow(kPapers, papers);
+      if(plusPapers * tempCurrentKPapers > minusPapers * (2 - tempCurrentKPapers))
       {
-        cash -= costPaper;        
-        papers++;
-        currentKPaper = Math.Pow(kPapers, papers);
-        costPaper *= (2 - currentKPaper);
-        ClearConsoleLine(enterRow);
-        Console.SetCursorPosition(enterCol, enterRow);
+        currentKPapers = tempCurrentKPapers;
       }
-      else
-      {
-        ClearConsoleLine(enterRow);
-        Console.SetCursorPosition(enterCol, enterRow);
+      costPapers *= (2 - currentKPapers);
+      ClearConsoleLine(enterRow);
+      Console.SetCursorPosition(enterCol, enterRow);
+    }
+    else
+    {
+      ClearConsoleLine(enterRow);
+      Console.SetCursorPosition(enterCol, enterRow);
 
-      }
-    //}
+    }
   }
 
-  static void CheckSalary()
+  static void CheckSalaryAndTaxes()
   {
     salary = 0;
-    for(int i = 0; i < obligations; i++)
-    {
-      salary += plusObligation * Math.Pow(kObligation, i);
-    }
-
-    for(int i = 0; i < papers; i++)
-    {
-      salary += plusPapers * Math.Pow(kPapers, i);
-    }
-  }
-
-  static void CheckTaxes()
-  {
     taxes = 0;
     for(int i = 0; i < obligations; i++)
     {
-      taxes += Math.Pow(kObligation, i) * minusObligation;
+      
+      double currentPlusObligation = plusObligation * Math.Pow(kObligation, i);
+      double currentMinusObligation = (2 - Math.Pow(kObligation, i)) * minusObligation;
+      
+      if(currentPlusObligation < currentMinusObligation)
+      {
+        currentPlusObligation = plusObligation * currentKObligation;
+        currentMinusObligation = minusObligation * (2 - currentKObligation);
+      }
+      salary += currentPlusObligation;
+      taxes += currentMinusObligation;
     }
 
     for(int i = 0; i < papers; i++)
     {
-      taxes += Math.Pow(kPapers, i) * minusPapers;
+      double currentPlusPapers = plusPapers * Math.Pow(kPapers, i);
+      double currentMinusPapers = (2 - Math.Pow(kPapers, i)) * minusPapers;
+      
+      if(currentPlusPapers < currentMinusPapers)
+      {
+        currentPlusPapers = plusPapers * currentKPapers;
+        currentMinusPapers = minusPapers * (2 - currentKPapers);
+      }
+      salary += currentPlusPapers;
+      taxes += currentMinusPapers;
     }
   }
+
 }
